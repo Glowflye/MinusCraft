980f48058b83e6ecc388ab4a5c92c5dad67eecf2
Tia <tiagsherriff@gmail.com>
2025-02-25 11:12:29 +0000

    Inventory Generation Started

--- Assets/Inventory Generation.cs
+++ Assets/Inventory Generation.cs
@@ -1,17 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryGeneration : MonoBehaviour
 {
-    private Object[] invItems;
+    private InventoryObject[] invItems; //Length should be 14 as that's the length of inventory
     public Canvas canvas;
+    private InventoryObject[] possibleItems;
+
+    private Image[] inventoryImages = { null, null, null };
+    private string[] inventoryNames = {"stick", "coal", "torch"}; //Add unique items later
+    private int[] inventoryValue = { 1, 1, 2 };
+
+    System.Random random = new System.Random();
+
     // Start is called before the first frame update
     void Start()
     {
+        //CREATE OBJECTS FROM EACH ITEM IN NAMES
+        for (int i = 0; i < inventoryImages.Length; i++) //Name, Image, Value, Amount
+        {
+            //int amount = random.Next(1, 32); //Not all objects can stack to 32 so change later --> move later
+            InventoryObject invObj = new InventoryObject(inventoryNames[i], inventoryImages[i], inventoryValue[i], 0); //0 should be set to value later
+            possibleItems[i] = invObj;
+        }
+
+        //CREATE OBJECTS FOR USE IN INV
         for (int i = 0; i < 14; i++)
         {
-
+            int genTrue = random.Next(0, 4);
+            if (genTrue == 0)
+            {
+                InventoryObject tempInvObj = possibleItems[random.Next(0, possibleItems.Length)]; //May need to change to Length - 1 later
+                tempInvObj.Amount = random.Next(1, 32); //Amount of inventory item generated here
+                invItems[i] = tempInvObj;
+            }
+            else
+            {
+                //Do nothing, there's only a 1/5 chance an item will generate in the inventory for now (may change later, playtest first)
+            }
         }
     }
 
--- Assets/InventoryBehaviour.cs
+++ Assets/InventoryBehaviour.cs
@@ -11,15 +11,17 @@ public class InventoryBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) == true && !isHandFull) {
+        if (Input.GetMouseButton(0) == true && !isHandFull)
         {
+            {
                 //itemInHand = ;
+            }
         }
     }
 }
